057960414d32b3aa6e7c347c8c78152de913c4bd
rlxa <rlxa@users.noreply.github.com>
2017-09-13 05:17:44 +0900

    Initial commit

---
+++ library/main.js
@@ -0,0 +1,24 @@
+﻿function prepareList() {
+	$('.explist').find('li:has(ul)').unbind('click').click( function(event) {
+		if (this == event.target) {
+			$(this).toggleClass('expanded');
+			$(this).children('ul').toggle('medium');
+		}
+		return false;
+	})
+	.addClass('collapsed').removeClass('expanded').children('ul').hide();
+	$('.explist a').unbind('click').click(function() {
+		window.location=$(this).attr('href');
+		return false;
+	});
+	$('#expand-list').unbind('click').click(function() {
+		$('.collapsed').addClass('expanded');
+		$('.collapsed').children('ul').show('medium');
+	});
+	$('#collapse-list').unbind('click').click(function() {
+		$('.collapsed').removeClass('expanded');
+		$('.collapsed').children('ul').hide('medium');
+	});
+};
+
+$(document).ready(function(){prepareList()});
